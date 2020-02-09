9b70bd7a8cd074ccc41366bb66988fbc7158e70f
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-09 05:12:00 +0000

    build based on c976c97

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/#CaimCore-API-1","page":"API Reference","title":"CaimCore API","text":"","category":"section"},{"location":"api/#","page":"API Reference","title":"API Reference","text":"CurrentModule = CaimCore","category":"page"},{"location":"#CaimCore.jl-1","page":"Home","title":"CaimCore.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Documentation for CaimCore.jl","category":"page"}]
+}
